28a47e6ce361c271394726753b3fe6e434603d13
John DeSilva <desilvjo@umich.edu>
2014-09-08 00:01:41 -0400

    Add canopy item listener

---
+++ src/main/scala/com/aesthetikx/android/canopy/CanopyItemListener.scala
@@ -0,0 +1,15 @@
+package com.aesthetikx.android.canopy
+
+import android.view.View
+import android.widget.{AdapterView, BaseAdapter}
+
+import java.util.List
+
+class CanopyItemListener(private val items: List[CanopyItem], private val adapter: BaseAdapter) extends AdapterView.OnItemClickListener {
+
+  override def onItemClick(parent: AdapterView[_], view: View, position: Int, id: Long) : Unit = {
+    adapter.getItem(position).asInstanceOf[CanopyItem].toggleExpanded
+    adapter.notifyDataSetChanged
+  }
+
+}
