490f81ffbca92ae6154efea0bd366e98173384c7
John DeSilva <desilvjo@umich.edu>
2014-09-09 11:16:35 -0400

    Add sample project

---
+++ sample/src/main/java/com/aesthetikx/android/canopy/sample/MainActivity.java
@@ -0,0 +1,14 @@
+package com.aesthetikx.android.canopy.sample;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
